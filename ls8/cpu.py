a95712b442d5995087645c22580a4d36659504bc
Rexben001 <rexben.rb@gmail.com>
2019-11-11 16:50:09 +0100

    update the run method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,8 +8,8 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 25
-        self.reg = []
+        self.ram = [0] * 255
+        self.reg = [0] * 8
         self.pc = 0
 
     def load(self):
@@ -64,10 +64,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        IR = self.ram[self.pc]
+        running = True
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        while running:
+            IR = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+
+            if IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == HLT:
+                running = False
 
     def ram_read(self, address):
         return self.ram[address]
 
     def ram_write(self, value, address):
-        pass
+        self.ram[address] = value
