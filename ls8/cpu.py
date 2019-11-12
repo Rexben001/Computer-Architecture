217190c45b64df246a8cbc6b7f0b7e5a7e757958
Rexben001 <rexben.rb@gmail.com>
2019-11-12 13:57:33 +0100

    implement multiply

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,33 +12,34 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
 
-    def load(self):
-        """Load a program into memory."""
+    # def load(self):
+    #     """Load a program into memory."""
 
-        address = 0
+    #     address = 0
 
-        # For now, we've just hardcoded a program:
+    #     # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+    #     program = [
+    #         # From print8.ls8
+    #         0b10000010,  # LDI R0,8
+    #         0b00000000,
+    #         0b00001000,
+    #         0b01000111,  # PRN R0
+    #         0b00000000,
+    #         0b00000001,  # HLT
+    #     ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+    #     for instruction in program:
+    #         self.ram[address] = instruction
+    #         address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -69,6 +70,7 @@ class CPU:
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
         while running:
             IR = self.ram[self.pc]
             operand_a = self.ram_read(self.pc+1)
@@ -76,15 +78,36 @@ class CPU:
 
             if IR == LDI:
                 self.reg[operand_a] = operand_b
-                self.pc += 3
+                self.pc += (IR >> 6) + 1
             elif IR == PRN:
                 print(self.reg[operand_a])
-                self.pc += 2
+                # mask IR to get the opcode 
+                self.pc += (IR >> 6) + 1
+            elif IR == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += (IR >> 6) + 1
             elif IR == HLT:
                 running = False
+                sys.exit(2)
 
     def ram_read(self, address):
         return self.ram[address]
 
-    def ram_write(self, value, address):
+    def ram_write(self, address, value):
         self.ram[address] = value
+
+    def load(self, filename):
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    if len(num) == 0:
+                        continue
+                    value = int(num, 2)
+                    self.ram[address] = value
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
